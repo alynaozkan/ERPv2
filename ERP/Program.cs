e86835e659dfbf8d24b8e93905e066b3d6840093
ladyjfuhrer <aleynaozkan90@gmail.com>
2025-01-05 20:51:40 +0300

    last1

--- ERP/Program.cs
+++ ERP/Program.cs
@@ -7,8 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddScoped(typeof(IRepository<,>), typeof(Repository<,>));
-
+builder.Services.AddScoped(typeof(IRepository<,>), typeof(Repository<,>)); 
 
 var app = builder.Build();
 
--- ERP/obj/Debug/net8.0/ERP.AssemblyInfo.cs
+++ ERP/obj/Debug/net8.0/ERP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00bce1d39b6788c5c5530e24306d930ae5a55077")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dade11a3b5aa1cf54350816fb860a3cd467fc471")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

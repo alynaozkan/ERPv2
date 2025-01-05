dade11a3b5aa1cf54350816fb860a3cd467fc471
ladyjfuhrer <aleynaozkan90@gmail.com>
2025-01-05 20:33:12 +0300

    last

--- ERP/obj/Debug/net8.0/ERP.AssemblyInfo.cs
+++ ERP/obj/Debug/net8.0/ERP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe9b11f0c3511b503439707179e176a78569b2dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00bce1d39b6788c5c5530e24306d930ae5a55077")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
